2561822060a5a1aaf621fefb3cd69d2f14645056
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-09-10 13:07:06 +0200

    String changes

--- Lucktext/Program.cs
+++ Lucktext/Program.cs
@@ -6,7 +6,33 @@ namespace Lucktext
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int start = 0;
+            Console.WriteLine("Skriv ett adjektiv");
+            string a1 = Console.ReadLine();
+            Console.WriteLine("Skriv ett adjektiv");
+            string a2 = Console.ReadLine();
+            System.Console.WriteLine("Skriv ett namn");
+            string a3 = Console.ReadLine();
+            System.Console.WriteLine("Skriv ett yrke");
+            string a4 = Console.ReadLine();
+            System.Console.WriteLine("Skriv ett verb");
+            string a5 = Console.ReadLine();
+            System.Console.WriteLine("skriv ett namn");
+            string a6 = Console.ReadLine();
+            System.Console.WriteLine("Skriv en åldersgrupp");
+            string a7 = Console.ReadLine();
+            System.Console.WriteLine("skriv en fritidsaktivitet");
+            string a8 = Console.ReadLine();
+            System.Console.WriteLine("Skriv ett djur");
+            string a9 = Console.ReadLine();
+            start = 1;
+
+            while (start == 1)
+            {
+
+            }
+
+
         }
     }
 }
